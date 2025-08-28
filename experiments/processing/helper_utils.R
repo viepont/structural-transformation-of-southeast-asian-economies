38c3095e9bf4121f65c1c2ea472a514c0781fe80
Vincent <vincepontines@gmail.com>
2025-08-28 09:40:21 +0800

    Added helper_utils.R, deleted misc. files in processing

---
+++ experiments/processing/helper_utils.R
@@ -0,0 +1,21 @@
+# Install the necessary packages
+install.packages(tidyverse)
+install.packages(readxl)
+install.packages(janitor)
+
+# Load libraries
+library(tidyverse)
+library(readxl)
+library(janitor)
+
+import_csv <- function(file_path) {
+  df <- read.csv(file_path, header = TRUE, sep = ",")
+  
+  # Clean column names
+  df <- janitor::clean_names(df)
+  
+  # Handle missing values
+  df <- df %>% drop_na()
+  
+  return(df)
+}
